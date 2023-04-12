f21e896cb1191d7b9a1670e9de6b4f5865885fae
unknown <bhotnikhil25@gmail.com>
2023-04-12 23:35:16 +0530

    [Nikhil]-Wel-Come To File IO Operation

---
+++ FileIOOperation/FileIOOperation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIOOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come To File IO operation");
+        }
+    }
+}
